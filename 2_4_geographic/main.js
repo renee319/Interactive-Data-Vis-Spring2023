725cbc7315da6683e3d7de050bf5206284c5e089
renee319 <yinyifu7@gmail.com>
2023-03-21 02:32:58 -0400

    do 2.3

--- 2_4_geographic/main.js
+++ 2_4_geographic/main.js
@@ -1,26 +1,56 @@
 /* CONSTANTS AND GLOBALS */
-// const width = window.innerWidth * 0.9,
-//   height = window.innerHeight * 0.7,
-//   margin = { top: 20, bottom: 50, left: 60, right: 40 };
+const width = window.innerWidth * 0.7,
+      height = window.innerHeight * 0.7,
+      margin = {top: 20, left: 60, bottom: 60, right: 20},
+      radius = 6;
 
-/**
- * LOAD DATA
- * Using a Promise.all([]), we can load more than one dataset at a time
- * */
- Promise.all([
-  d3.json("../data/usState.json"),
-  d3.csv("../data/stateCapitals.csv", d3.autoType),
-]).then(([geojson, capitals]) => {
+/* LOAD DATA */
+d3.csv("../data/usHeatExtremes.csv", d3.autoType)
+  .then(data => {
+  console.log(data, )
   
-  // SPECIFY PROJECTION
- 
+  /* SCALES */
+  const xScale = d3.scaleLinear()
+  .domain([Math.min(...data.map((d => d.Lat))), Math.max(...data.map((d => d.Lat)))])
+  .range([margin.left, width - margin.right])
 
-  // DEFINE PATH FUNCTION
-
-
-  // APPEND GEOJSON PATH  
+  const yScale = d3.scaleLinear()
+  .domain([Math.min(...data.map((d => d.Long))), Math.max(...data.map((d => d.Long)))])
+  .range([height - margin.bottom, margin.top])
   
-  
-  // APPEND DATA AS SHAPE
+  // Color scale
+  const colorScale = d3.scaleOrdinal()
+  // red changes more, green changes less, blue is ocean
+  .domain([Math.min(...data.map((d => d["Change in 95 percent Days"]))), Math.max(...data.map((d => d["Change in 95 percent Days"])))])
+  .range(["#0000ff", "#0011ee", "#0022dd","#0033cc","#0044bb","#0055aa","#006699","#007788", "#008877","#009966","#00aa55","#00bb44","#00cc33", "#00dd22", "#00ff11"].reverse())
 
+  const radiusScale = d3.scaleLinear()
+  .domain([Math.min(...data.map((d => d["Change in 95 percent Days"]))), Math.max(...data.map((d => d["Change in 95 percent Days"])))])
+  .range([4, 8])
+
+  /* HTML ELEMENTS */
+  const svg = d3.select("#container")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .style("background-color", "Brown")
+
+  const dots = svg.selectAll(".dot")
+  .data(data)
+  .join("circle")
+  .join(
+    enter => enter.append("circle")
+    .call(selection => 
+      selection
+      .transition()
+      .duration(1000)
+      .attr("r", radius)),
+    update => update,
+    exit => exit
+  )
+  .attr("class", "dot")
+  .attr("r", d => radiusScale( d["Change in 95 percent Days"]))
+  .attr("cx", d => xScale(d.Lat))
+  .attr("cy", d => yScale(d.Long))
+  .style("fill", d => colorScale(d["Change in 95 percent Days"]))
 });
