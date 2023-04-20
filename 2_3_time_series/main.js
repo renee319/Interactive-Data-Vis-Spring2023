db1b3367e26d103ccbfb4835669c842279d55edf
renee319 <yinyifu7@gmail.com>
2023-04-20 01:12:47 -0400

    use another dataset!

--- 2_3_time_series/main.js
+++ 2_3_time_series/main.js
@@ -1,45 +1,43 @@
 /* CONSTANTS AND GLOBALS */
 const width = window.innerWidth * 0.7,
   height = window.innerHeight * 0.7,
-  margin = { left: 20, top: 60, bottom: 60, right: 20};
+  margin = { left: 20, top: 60, bottom: 50, right: 20};
 
 /* LOAD DATA */
-d3.csv('../data/squirrelActivities.csv',  d3.autoType)
+d3.csv('../data/afganistanOverTime.csv',  d3.autoType)
 .then(data => {
-  console.log('data :>> ', data);
+  console.log('data :>> ', data)
   // SCALES  
-  const xScale = d3.scaleBand()
-    .domain([...data.map((d => d.activity))])
-    .range([margin.left, height - margin.right])
-    .padding(0.1)
-  
+  const xScale = d3.scaleTime()
+    .domain(d3.extent(data, d => new Date( d.Year, 0)))
+    .range([margin.left, width - margin.right])
+
   // Y Scale
   const yScale = d3.scaleLinear()
-  .domain(d3.extent(data, d => d.count))
-  .range([height - 2.3*margin.bottom, margin.top])
+  .domain(d3.extent(data, d => d.Population))
+  .range([height - margin.bottom, margin.top])
 
   // CREATE SVG ELEMENT
   const svg = d3.select("#container")
     .append("svg")
     .attr("width", width)
     .attr("height", height)
-  
   // BUILD AND CALL AXES
   // const filteredData = data.filter(d => d.country === "US")
   // console.log("filtered", filteredData)
-
   // LINE GENERATOR FUNCTION
   const lineGen = d3.line()
-    .x(d => xScale(d.activity))
-    .y(d => yScale(d.count))
+    .x(d => xScale(new Date(d.Year, 0)))
+    .y(d => yScale(d.Population))
   
   const areaGen = d3.area()
-  .x(d => xScale(d.activity))
-  .y1(d => yScale(d.count))
-  .y0(yScale(0))
+  .x(d => xScale(new Date(d.Year, 0)))
+  .y1(d => yScale(d.Population))
+  .y0(d3.extent(data, d => d.Population)[0])
 
   // Group Data
-  const groupedData = d3.groups(data, d => d.country)
+  const groupedData = d3.groups(data, d => d.Entity)
+  console.log(groupedData)
   
   // DRAW LINE
   const line = svg.selectAll(".area")
@@ -50,9 +48,8 @@ d3.csv('../data/squirrelActivities.csv',  d3.autoType)
     .attr("stroke", "black")
     .attr("fill", "#94877C")
 
-    const xAxis = d3.axisBottom(xScale)
-
-    svg.append("g")
-    .style("transform", `translate(${-2.7 * margin.right}px, ${height - 0.5*margin.bottom}px`)
+  const xAxis = d3.axisBottom(xScale)
+  svg.append("g")
+    .style("transform", `translate(0px, ${height - margin.bottom}px)`)
     .call(xAxis)
 });
